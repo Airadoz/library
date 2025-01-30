7076be677619521094ee1d028654d4b09f664a3c
Airadoz <airadozik@gmail.com>
2025-01-30 22:47:22 +0300

    Added css resets, added modal window, added modal showing

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const myLibrary = [];
+const modal = document.querySelector(".modal");
+const openModal = document.querySelector(".open-modal");
+const closeModal = document.querySelector(".close-modal");
+
+openModal.addEventListener("click", ()=>{
+    modal.showModal();
+})
+closeModal.addEventListener("click", ()=>{
+    modal.close();
+})
 
 function Book(title, author, pages, read) {
   this.title = title;
