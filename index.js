c3911539c787cfe43fc7363f3041ff595ae7ba94
Airadoz <airadozik@gmail.com>
2025-01-31 23:00:22 +0300

    added variables for form inputs

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const modal = document.querySelector(".modal");
 const openModal = document.querySelector(".open-modal");
 const closeModal = document.querySelector(".close-modal");
 
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.getElementById("pages");
+const book_read = document.getElementById("read");
+
 openModal.addEventListener("click", ()=>{
-    modal.showModal();
+  modal.showModal();
 })
 closeModal.addEventListener("click", ()=>{
-    modal.close();
+  modal.close();
 })
 
 function Book(title, author, pages, read) {
