41edcac4f280beac9f0a3c0fc5728e6f4fe79009
Airadoz <airadozik@gmail.com>
2025-02-02 16:48:07 +0500

    created new js file, setted variables for modal and inputs

---
+++ test.js
@@ -0,0 +1,8 @@
+const modal = document.querySelector(".modal");
+const openModal = document.querySelector(".open-modal");
+const closeModal = document.querySelector(".close-modal");
+
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.getElementById("pages");
+const book_read = document.getElementById("read");
